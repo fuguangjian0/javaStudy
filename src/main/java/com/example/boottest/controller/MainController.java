339ef5e78d30cd8ac8d2983793e5f9dd77a79db6
fuguangjian0 <fuguangjian0@gmail.com>
2023-04-05 19:33:25 +0800

    add controller

---
+++ src/main/java/com/example/boottest/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.boottest.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * This class is for xxxx.
+ *
+ * @author Administrator
+ * @version 2023/4/5 19:32
+ */
+@Controller
+public class MainController {
+}
